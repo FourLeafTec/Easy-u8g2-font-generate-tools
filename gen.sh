b87abeac4052fc6684d5b7b385cc00a1e7c05364
fourleaf <gundamwing.wei@gmail.com>
2025-04-30 15:05:36 +0800

    自用字体

---
+++ gen.sh
@@ -0,0 +1,26 @@
+python3 main.py \
+    -i "0123456789MV." \
+    -n "Sarasa14" \
+    -p "font/SarasaMonoSC-Regular.ttf" \
+    -d 72 \
+    -s 14 \
+    -sp 0 \
+    -b -c -ia
+
+python3 main.py \
+    -i "加载中." \
+    -n "wenquanyi" \
+    -p "font/wenquanyi.ttf" \
+    -d 72 \
+    -s 16 \
+    -sp 0 \
+    -c
+
+python3 main.py \
+    -i "0123456789MVv. 输入密码电压低保护保存并退出电机过流保护福祉座椅控制器传输" \
+    -n "wenquanyi" \
+    -p "font/wenquanyi.ttf" \
+    -d 72 \
+    -s 14 \
+    -sp 0 \
+    -c
